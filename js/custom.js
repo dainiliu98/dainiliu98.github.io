eb17985c03b711cde1d8b6e811756346ab1f06ab
Daini Liu <dannieliu@dainis-mbp.student.wheaton.edu>
2020-01-21 23:54:52 -0600

    changes

--- js/custom.js
+++ js/custom.js
@@ -82,7 +82,8 @@
    var typed = $(".typed");
    $(function () {
      typed.typed({
-       strings: ["Daini Liu.", "a College Senior.", "a Graphic Designer.", "a Photographer.", "a Biologist.", "a Christ Follower."],
+       strings: ["Daini Liu.", "a College Senior.", "a Graphic Designer.", "a Photographer.", "an aspiring UX designer","a Biologist.", "a Christ Follower."
+       ],
        typeSpeed: 50,
        loop: true,
      });
